0956bafc6526fc2ebdec3e9f0a3078f0f91e26dc
AndreyPechterev <andreypechterev1234@mail.ru>
2022-11-07 11:39:19 +0300

    Initial commit

---
+++ src/main/java/web/dao/UserDao.java
@@ -0,0 +1,19 @@
+package web.dao;
+
+import web.entity.User;
+import java.util.List;
+
+public interface UserDao {
+
+    public void addUser(User user);
+
+    public void changeUser(int id, User changedUser);
+
+    public void deleteUser(int id);
+
+    public List<User> showAllUsers();
+
+    public User getUserById(int id);
+
+
+}
---
+++ src/main/java/web/service/UserService.java
@@ -0,0 +1,18 @@
+package web.service;
+
+import web.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    public void addUser(User user);
+
+    public void changeUser(int id, User changedUser);
+
+    public void deleteUser(int id);
+
+    public List<User> showAllUsers();
+
+    public User getUserById(int id);
+}
